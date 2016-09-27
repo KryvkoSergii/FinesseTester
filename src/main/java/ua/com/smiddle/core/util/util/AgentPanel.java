347629022785cb860dc3a8af7044f8527f21ad48
kryvkosergii <kryvkosergii@gmail.com>
2016-09-27 19:13:20 +0300

    multi user mode

--- src/main/java/ua/com/smiddle/core/util/util/AgentPanel.java
+++ src/main/java/ua/com/smiddle/core/util/util/AgentPanel.java
@@ -1,7 +1,10 @@
 package ua.com.smiddle.core.util.util;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import ua.com.smiddle.core.util.model.interfaces.Action;
 
@@ -12,29 +15,52 @@ import ua.com.smiddle.core.util.model.interfaces.Action;
 public class AgentPanel extends JPanel {
     private String loginId;
     private Action state;
-    private JLabel lblTitle1;
+    private JLabel lblTitle1 = new JLabel("Agent ID:");
+    private JLabel lblTitle2 = new JLabel("CURRENT STATE:");
     private JLabel agentName;
     private JLabel agentState;
-    private LayoutManager layout;
+    private AgentPanel panel;
 
     public AgentPanel() {
-
     }
 
     public AgentPanel(String loginId, Action state) {
         this.loginId = loginId;
         this.state = state;
+        init();
+    }
+
+
+    public Action getState() {
+        return state;
+    }
+
+    public void setState(Action state) {
+        this.state = state;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
     }
 
     private void init() {
-        layout = new FlowLayout();
-        this.setLayout(layout);
-        lblTitle1 = new JLabel("Agent ID:");
-        agentName = new JLabel("...");
+        FlowLayout layout = new FlowLayout();
+        layout.setAlignment(FlowLayout.LEFT);
+        setLayout(layout);
+        agentName = new JLabel(loginId);
         agentState = new JLabel(Action.UNKNOWN.name());
-        this.add(lblTitle1,0);
-        this.add(agentName,0);
-        this.add(agentState,0);
+        panel = this;
+        panel.add(lblTitle1,0);
+        panel.add(agentName,1);
+        panel.add(lblTitle2,2);
+        panel.add(agentState,3);
+        panel.setVisible(true);
+        panel.setBorder(new LineBorder(Color.BLACK));
+        this.setSize(400,70);
     }
 
 
--- src/main/java/ua/com/smiddle/core/util/util/MultiUserForm.java
+++ src/main/java/ua/com/smiddle/core/util/util/MultiUserForm.java
@@ -1,33 +1,63 @@
 package ua.com.smiddle.core.util.util;
 
-import ua.com.smiddle.core.util.model.interfaces.*;
 import ua.com.smiddle.core.util.model.interfaces.Action;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author srg on 27.09.16.
  * @project FinesseTester
  */
-public class MultiUserForm extends JFrame{
+public class MultiUserForm extends JFrame implements PropertyChangeListener {
+    PropertyChangeSupport propertyChangeSupport;
     private JButton btnAdd;
-    private JPanel contentPane;
+    private JPanel panel;
+    private JPanel rootPanel;
+    private JScrollPane container;
+    private JPanel forContents;
     private MultiUserForm form;
+    private Map<String, AgentPanel> panelMap = new ConcurrentHashMap<>();
 
     public MultiUserForm() {
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        setSize(850, 300);
+        setSize(400, 300);
+        setContentPane(rootPanel);
         form = this;
+        forContents.setLayout(new FlowLayout());
+        container.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        container.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        container.setViewportBorder(new LineBorder(Color.BLACK));
+        container.getViewport().add(forContents);
+        propertyChangeSupport = new PropertyChangeSupport(this);
+        propertyChangeSupport.addPropertyChangeListener(this);
         btnAdd.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String loginId = JOptionPane.showInputDialog(form,"INPUT AGENT NAME");
-                AgentPanel agentPanel = new AgentPanel(loginId, Action.UNKNOWN);
-                contentPane.add(agentPanel);
-
+                String loginId = JOptionPane.showInputDialog(form, "Input agent name");
+                propertyChangeSupport.firePropertyChange("new_component", null, new AgentPanel(loginId, Action.UNKNOWN));
             }
         });
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if (evt.getPropertyName().equals("new_component")) {
+            if (evt.getNewValue() instanceof AgentPanel) {
+                AgentPanel agentPanel = (AgentPanel) evt.getNewValue();
+                panelMap.put(agentPanel.getLoginId(), agentPanel);
+                forContents.add(agentPanel);
+                forContents.revalidate();
+                forContents.repaint();
+            }
+        }
     }
 }
